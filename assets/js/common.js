1e6c9dd22157f13cd8897732e34e67d99c09e84e
*dJdCucEGeo <msipod3394@gmail.com>
2024-03-27 21:08:41 +0900

    header toggle実装

---
+++ assets/js/common.js
@@ -0,0 +1,23 @@
+// header
+$(document).ready(function () {
+  $(".js-hover-service").hover(
+    function () {
+      console.log("hover!");
+      $(".js-hover-service").addClass("_isShow");
+    },
+    function () {
+      console.log("hover out!");
+      $(".js-hover-service").removeClass("_isShow");
+    }
+  );
+  $(".js-hover-company").hover(
+    function () {
+      console.log("hover!");
+      $(".js-hover-company").addClass("_isShow");
+    },
+    function () {
+      console.log("hover out!");
+      $(".js-hover-company").removeClass("_isShow");
+    }
+  );
+});
