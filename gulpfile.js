b8de72879afb00ed39e225afb661e7e46a35db59
*dJdCucEGeo <msipod3394@gmail.com>
2024-04-09 12:04:45 +0900

    gulp 設定変更

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,21 @@
 const gulp = require("gulp");
 const sass = require("gulp-dart-sass");
 
+// 動的インポートを使用して gulp-filter をインポート
+const filterPromise = import("gulp-filter");
+
 // SCSSをコンパイルするタスク
 gulp.task("sass", function () {
-  return gulp
-    .src("./src/scss/**/*.scss") // 入力元のSCSSファイルのパス
-    .pipe(sass().on("error", sass.logError)) // SCSSをコンパイル
-    .pipe(gulp.dest("./assets/css")); // 出力先のディレクトリ
+  // '_'を含むファイルを除外するためのフィルターを作成
+  return filterPromise.then(({ default: filter }) => {
+    const excludeUnderscoreFiles = filter(["**", "!**/_*.scss"]);
+
+    return gulp
+      .src("./src/scss/**/*.scss") // 入力元のSCSSファイルのパス
+      .pipe(excludeUnderscoreFiles) // '_'を含むファイルを除外
+      .pipe(sass().on("error", sass.logError)) // SCSSをコンパイル
+      .pipe(gulp.dest("./assets/css")); // 出力先のディレクトリ
+  });
 });
 
 // ファイルの変更を監視して自動的にコンパイルするタスク
