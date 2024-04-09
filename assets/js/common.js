6567877cef6f991197de5215e39f75a6e5c042d2
*dJdCucEGeo <msipod3394@gmail.com>
2024-04-09 11:40:21 +0900

    header sp化

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,55 +1,45 @@
-// サービス
-$(document).ready(function () {
-  $(".js-hover-service").hover(
-    function () {
-      console.log("hover!");
-      $(".js-hover-service").addClass("_isShow");
-    },
-    function () {
-      console.log("hover out!");
-      $(".js-hover-service").removeClass("_isShow");
-    }
-  );
-
-  $(".js-hover-company").hover(
-    function () {
-      console.log("hover!");
-      $(".js-hover-company").addClass("_isShow");
-    },
-    function () {
-      console.log("hover out!");
-      $(".js-hover-company").removeClass("_isShow");
-    }
-  );
-});
+// ウィンドウの幅が指定された幅以上であるかをチェックする関数
+function isWindowWidthAbove(width) {
+  return $(window).width() >= width;
+}
 
 // ヘッダーの処理
 $(document).ready(function () {
   var lastScrollTop = 0;
   var header = $(".header");
 
-  $(window).scroll(function (event) {
-    var st = $(this).scrollTop();
-    if (st > lastScrollTop) {
-      // 下にスクロールしている場合
-      if (header.hasClass("scrolled-top")) {
-        header.addClass("_remove");
-        header.fadeOut(500, function () {
-          header.removeClass("scrolled-top").fadeIn();
-          setTimeout(() => {
-            header.removeClass("_remove");
-          }, 500);
-        });
+  if (isWindowWidthAbove(1200)) {
+    $(window).scroll(function (event) {
+      var st = $(this).scrollTop();
+      if (st > lastScrollTop) {
+        // 下にスクロールしている場合
+        if (header.hasClass("scrolled-top")) {
+          header.addClass("_remove");
+          header.fadeOut(500, function () {
+            header.removeClass("scrolled-top").fadeIn();
+            setTimeout(() => {
+              header.removeClass("_remove");
+            }, 500);
+          });
+        }
+      } else {
+        // 上にスクロールしている場合
+        header.addClass("scrolled-top");
       }
-    } else {
-      // 上にスクロールしている場合
-      header.addClass("scrolled-top");
-    }
 
-    if ($(".mv").isOnScreen()) {
-      header.removeClass("scrolled-top");
-    }
+      if ($(".mv").isOnScreen()) {
+        header.removeClass("scrolled-top");
+      }
 
-    lastScrollTop = st;
-  });
+      lastScrollTop = st;
+    });
+  }
+});
+
+//ハンバーガーメニュー
+const ham = $("#js-hamburger");
+const nav = $("#header");
+ham.on("click", function () {
+  ham.toggleClass("_active"); // ハンバーガーメニューにactiveクラスを付け外し
+  nav.toggleClass("_active"); // ナビゲーションメニューにactiveクラスを付け外し
 });
