62884867113f1c19f337b85a07b625ae84d72987
*dJdCucEGeo <msipod3394@gmail.com>
2024-04-09 10:46:32 +0900

    mv, header周り

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,6 +1,5 @@
-// header
+// サービス
 $(document).ready(function () {
-
   $(".js-hover-service").hover(
     function () {
       console.log("hover!");
@@ -22,5 +21,35 @@ $(document).ready(function () {
       $(".js-hover-company").removeClass("_isShow");
     }
   );
+});
 
+// ヘッダーの処理
+$(document).ready(function () {
+  var lastScrollTop = 0;
+  var header = $(".header");
+
+  $(window).scroll(function (event) {
+    var st = $(this).scrollTop();
+    if (st > lastScrollTop) {
+      // 下にスクロールしている場合
+      if (header.hasClass("scrolled-top")) {
+        header.addClass("_remove");
+        header.fadeOut(500, function () {
+          header.removeClass("scrolled-top").fadeIn();
+          setTimeout(() => {
+            header.removeClass("_remove");
+          }, 500);
+        });
+      }
+    } else {
+      // 上にスクロールしている場合
+      header.addClass("scrolled-top");
+    }
+
+    if ($(".mv").isOnScreen()) {
+      header.removeClass("scrolled-top");
+    }
+
+    lastScrollTop = st;
+  });
 });
---
+++ assets/js/top.js
@@ -0,0 +1,84 @@
+// プログレスバーの要素を取得
+const bar = document.querySelector(".mv__progressbar--in");
+
+// オートプレイの速度を設定
+const speed = 5000;
+
+// ページの読み込みが完了したときに実行される処理
+window.onload = function () {
+  // Swiperインスタンスを作成
+  const swiper = new Swiper(".swiper", {
+    loop: true, // ループを有効化
+    effect: "fade", // フェード切り替え
+    // オートプレイ設定
+    autoplay: {
+      delay: speed, // オートプレイの遅延
+      disableOnInteraction: false, // ユーザーが操作してもオートプレイを継続
+    },
+    // スライド変更時のイベントリスナーを設定
+    on: {
+      // Swiperが初期化されたときの処理
+      init: function () {
+        // プログレスバーのアニメーションを開始し、100%に拡大
+        bar.style.transitionDuration = speed + "ms";
+        bar.style.transform = "scaleX(1)";
+      },
+      // スライド変更のトランジションが開始したときの処理
+      slideChangeTransitionStart: function () {
+        // プログレスバーのアニメーションを停止し、0%にリセット
+        bar.style.transitionDuration = "0s";
+        bar.style.transform = "scaleX(0)";
+      },
+      // スライド変更のトランジションが終了したときの処理
+      slideChangeTransitionEnd: function () {
+        // プログレスバーのアニメーションを再開し、100%に拡大
+        bar.style.transitionDuration = speed + "ms";
+        bar.style.transform = "scaleX(1)";
+      },
+    },
+  });
+};
+
+// jQueryの拡張機能: 要素がビューポート内にあるかどうかを判定する関数
+// jQueryの拡張機能: 要素がビューポート内にあるかどうかを判定する関数
+$.fn.isOnScreen = function () {
+  var win = $(window);
+  var viewport = {
+    top: win.scrollTop(),
+    left: win.scrollLeft(),
+  };
+  viewport.right = viewport.left + win.width();
+  viewport.bottom = viewport.top + win.height();
+  var bounds = this.offset();
+  bounds.right = bounds.left + this.outerWidth();
+  bounds.bottom = bounds.top + this.outerHeight();
+  return !(
+    viewport.right < bounds.left ||
+    viewport.left > bounds.right ||
+    viewport.bottom < bounds.top ||
+    viewport.top > bounds.bottom
+  );
+};
+
+$(document).ready(function () {
+  // ページ読み込み時にも実行
+  checkElementVisibility();
+
+  // スクロールイベントを設定
+  $(window).scroll(function () {
+    // スクロール時にも実行
+    checkElementVisibility();
+  });
+});
+
+function checkElementVisibility() {
+  if ($(".mv").isOnScreen()) {
+    // 要素がビューポート内にある場合
+    $(".header").addClass("_notfixed");
+    $(".header").removeClass("scrolled-top");
+  } else {
+    // 要素がビューポート内にない場合
+    $(".header").removeClass("_notfixed");
+  }
+}
+
